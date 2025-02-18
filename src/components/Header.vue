9e3ff6ec5db7e0b59602e158b96bcf1bd7e88ec7
MekanEs <Mekan.es1997@gmail.com>
2025-02-18 14:17:15 +0300

    formatting css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ButtonType } from "../types/types";
-import Button from "./Button.vue";
-import Logo from "./Logo.vue";
-import NavBar from "./NavBar.vue";
+import { ButtonType } from '../types/types';
+import Button from './Button.vue';
+import Logo from './Logo.vue';
+import NavBar from './NavBar.vue';
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import NavBar from "./NavBar.vue";
     <NavBar />
     <div class="header__button-container">
       <Button>Contact Us</Button>
-      <Button :type="ButtonType.GRADIENT">Join Hydra</Button>
+      <Button :type="ButtonType.GRADIENT"> Join Hydra </Button>
     </div>
   </div>
 </template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import Link from './Link.vue';
-
 </script>
 
 <template>
-    <div class="navbar">
+  <div class="navbar">
     <Link class-name="navbar__link" path="#about">About</Link>
     <Link class-name="navbar__link" path="#service">Services</Link>
     <Link class-name="navbar__link" path="#technologies">Technologies</Link>
     <Link class-name="navbar__link" path="#how_to">How to</Link>
-    </div>
+  </div>
 </template>
